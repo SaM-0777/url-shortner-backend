007c5d72f27c5eb605711cff0e0df4ebe395bc93
Soumyaranjan Sahu <sam.2.3.4.5.ranjan@gmail.com>
2023-01-05 23:11:08 +0530

    JWT verify:<JWT Invalid signature error> unknown issue

--- src/middlewares/api/AuthorizeRequest.ts
+++ src/middlewares/api/AuthorizeRequest.ts
@@ -2,25 +2,17 @@ import express from "express";
 import jwt from "jsonwebtoken";
 
 
-const JWT_SECRET = "adwAS3D54119243t18n6fyt13i834z311R6TX36" as string;
-const JWT_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2I2YWZkMjUxNGU5MWE3OTcyYTZkOTIiLCJpYXQiOjE2NzI5MjkzMjh9.1AtAM5rjeuu6wMHoleZfrL5B9wTCPPvCkdP2R8cQrmo"
+const JWT_SECRET = process.env.JWT_SECRET as string;
 
 export default async function authorizeRequest(req:express.Request, res: express.Response, next: express.NextFunction) {
   const token = req.header('auth-token') as string
   console.log(token)
   if(!token) res.status(403).json({ RequestAuth: "Access Denied" })
   
-  jwt.verify(
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2I2YWZkMjUxNGU5MWE3OTcyYTZkOTIiLCJpYXQiOjE2NzI5MzkzMjV9.A_k-NiBGWcmlUlb1X8vkAoCg6Lf3_6W6ubVbeKfgfQU",
-    "adwAS3D54119243t18n6fyt13i834z311R6TX36",
-    (error, verified) => {
-      console.log(error)
-      if (error)
-        return res
-          .status(400)
-          .json({ TokenVerificationError: "Token could not be verified" });
-      next();
-    }
-  );
+  jwt.verify(token, JWT_SECRET, (error, verified) => {
+    console.log(error)
+    if (error) return res.status(400).json({ TokenVerificationError: "Token could not be verified" })
+    next()
+  })
 };
 
