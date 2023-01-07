004b515b9103c3a3489e8358d221ffad1a6aa329
Soumyaranjan Sahu <sam.2.3.4.5.ranjan@gmail.com>
2023-01-07 11:53:37 +0530

    env vars in production

--- src/middlewares/api/AuthorizeRequest.ts
+++ src/middlewares/api/AuthorizeRequest.ts
@@ -12,7 +12,7 @@ const JWT_SECRET = process.env.JWT_SECRET as string;
 export default async function authorizeRequest(req:express.Request, res: express.Response, next: express.NextFunction) {
   const token = req.header('auth-token') as string
   if(!token) return res.status(403).json({ RequestAuth: "Access Denied" })
-  // console.log(JWT_SECRET, typeof JWT_SECRET)
+  console.log(JWT_SECRET, typeof JWT_SECRET)
   // async
   jwt.verify(token, JWT_SECRET, (error, verified) => {
     if (error) return res.status(400).json({ TokenVerificationError: "Token could not be verified" })
